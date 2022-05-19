9915dc6bb1a5461e1ddbf0ef979a519fc3820367
adejar12 <adejarjunior18@gmail.com>
2022-05-18 21:34:46 -0300

    chore: removing test lines

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -6,13 +6,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./product-create.component.css"],
 })
 export class ProductCreateComponent implements OnInit {
-  atributoLog = "um valor por ai";
-
   constructor() {}
 
   ngOnInit(): void {}
-
-  fazerAlgo(): void {
-    console.log("Fazendo Algo");
-  }
 }
